7a4d5c6c1e42e7f5910b9eadd05133792376bc5e
mins0o0ng <135709442+mins0o0ng@users.noreply.github.com>
2024-06-27 10:07:53 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import PhotoUploader from './PhotoUploader';
+
+function App() {
+  return (
+    <div>
+      <PhotoUploader />
+    </div>
+  );
+}
+
+export default App;
